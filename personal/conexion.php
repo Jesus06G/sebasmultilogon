5780a7abccbd9b4ac5f7a05eb9c652829d74794a
Jesus <jdgonzalez815@misena.edu.co>
2022-10-31 11:58:50 -0500

    dsakpodas

---
+++ personal/conexion.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'php_multilogin',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
+	
+?>
